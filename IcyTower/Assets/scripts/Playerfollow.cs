3067f3696fe3d264b1bdadc690fd56a236f374d7
adigol <35373818+adigol@users.noreply.github.com>
2020-04-27 17:08:49 +0300

    The main camera follows the character

---
+++ IcyTower/Assets/scripts/Playerfollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerfollow : MonoBehaviour {
+
+	public Transform PlayerTransform;
+	private Vector3 _cameraOffset;
+	[Range(0.01f,1.0f)]
+	public float smoothFactor = 0.5f;
+
+	public bool LookAtPlayer = false;
+	// Use this for initialization
+	void Start () {
+		_cameraOffset = transform.position - PlayerTransform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPos = PlayerTransform.position + _cameraOffset;
+		transform.position = Vector3.Slerp (transform.position, newPos, smoothFactor);
+		if (LookAtPlayer) {
+			transform.LookAt (PlayerTransform);
+		}
+	}
+}
