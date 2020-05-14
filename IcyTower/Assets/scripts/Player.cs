a83e1a2360b3502e5777783395f7ca862893953f
izhakz75 <44816780+izhakz75@users.noreply.github.com>
2020-05-14 14:17:29 +0300

    restart game when player is falling

--- IcyTower/Assets/scripts/Player.cs
+++ IcyTower/Assets/scripts/Player.cs
@@ -1,25 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour {
 
 	float maxPos;
-
+	float minPos;
+	private Scene scene;
 	// Use this for initialization
 	void Start () {
 		maxPos = 7f;
+		minPos = 0f;
+		scene = SceneManager.GetActiveScene ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (maxPos < this.transform.position.y) {
+		if (maxPos < this.transform.position.y) 	// detect player reach new height
+		{
 			maxPos = this.transform.position.y;
 //			Debug.Log ("player max pos: " + maxPos.ToString ());
+			minPos = maxPos - 7;
+		}
+
+		if (this.transform.position.y < minPos) 	// detect falling
+		{
+			Debug.Log ("game over");
+			Application.LoadLevel (scene.name);     // restart the game whene the player is falling
 		}
 	}
 
 	public float GetMaxPos(){
 		return maxPos;
+	}
+		
+	public float GetMinPos(){
+		return minPos;
 	}
 }
