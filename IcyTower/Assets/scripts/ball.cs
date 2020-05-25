c20d2975adf8bc955dc2888e536c55c443bc2067
izhakz75 <44816780+izhakz75@users.noreply.github.com>
2020-05-25 12:58:01 +0300

    start adding ball

---
+++ IcyTower/Assets/scripts/ball.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballRolling : MonoBehaviour {
+
+	public float speed = 2f;
+	private Rigidbody ball;
+	// Use this for initialization
+	void Start () {
+		ball = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float movementHor = Input.GetAxis ("Horizontal");
+	}
+}
