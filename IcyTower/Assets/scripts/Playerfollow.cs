3760da9ee9cf634f07b6421da3d23d2e462c5c3c
izhakz75 <44816780+izhakz75@users.noreply.github.com>
2020-06-22 13:38:24 +0300

    planar movement + camera follow

--- IcyTower/Assets/scripts/Playerfollow.cs
+++ IcyTower/Assets/scripts/Playerfollow.cs
@@ -6,22 +6,34 @@ public class Playerfollow : MonoBehaviour {
 
 	public Transform PlayerTransform;
 	private Vector3 _cameraOffset;
+	private Vector3 camPos;
 	//[Range(0.01f,1.0f)]
 	public float smoothFactor = 0.5f;
-
+	[SerializeField] private Ball ball;
 	public bool LookAtPlayer = false;
+	[SerializeField] private float distAway;
+	[SerializeField] private float distUp;
+	[SerializeField] private float smooth;
+	private Transform follow;
 	// Use this for initialization
 	void Start () {
-		_cameraOffset = transform.position - PlayerTransform.position;
+//		_cameraOffset = transform.position - PlayerTransform.position;
+		follow = GameObject.FindWithTag ("Ball").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 newPos = PlayerTransform.position + _cameraOffset;
-		newPos += new Vector3 (0f, -5f, 5f);
-		transform.position = Vector3.Slerp (transform.position, newPos, smoothFactor);
-		if (LookAtPlayer) {
-			transform.LookAt (PlayerTransform);
-		}
+//		Vector3 newPos = PlayerTransform.position + _cameraOffset;
+//		newPos += new Vector3 (0f, -5f, 5f);
+//		transform.position = Vector3.Slerp (transform.position, newPos, smoothFactor);
+//		if (LookAtPlayer) {
+//			transform.LookAt (PlayerTransform);
+//		}
+	}
+
+	void LateUpdate(){
+		camPos = follow.position + Vector3.up * distUp + -distAway*ball.GetForwardDir ();
+		transform.position = Vector3.Lerp (transform.position, camPos, Time.deltaTime * smooth);
+		transform.LookAt (follow);
 	}
 }
--- IcyTower/Assets/scripts/ball.cs
+++ IcyTower/Assets/scripts/ball.cs
@@ -9,12 +9,15 @@ public class Ball : MonoBehaviour {
 	float minPos;
 	private Scene scene;
 
-	public float speed = 2f;
+	[SerializeField] private float force = 2f;
 	public float jumpForce = 10f;
 	private Rigidbody ball;
 	private bool onGround = false;
 	private Renderer ballRenderer;
 	Color ballCol = Color.red;
+	private Vector3 forwardDir;
+	private Vector3 leftDir;
+	private Vector3 rightDir;
 	// Use this for initialization
 	void Start () {
 		ball = GetComponent<Rigidbody> ();
@@ -22,22 +25,54 @@ public class Ball : MonoBehaviour {
 		minPos = 0f;
 		scene = SceneManager.GetActiveScene ();
 		ballRenderer = this.GetComponent<Renderer> ();
+		forwardDir = new Vector3(1,0,0);
+		rightDir = new Vector3(0,0,-1);
+		leftDir = new Vector3(0,0,1);
+		ball.velocity = Vector3.zero;
 	}
 
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Update () {
 		
-		if (onGround){
-			if (Input.GetButtonDown ("Jump")) {
-				ball.AddForce (Vector3.up * jumpForce, ForceMode.Impulse);
-				onGround = false;
-			}
-		
+//		if (onGround){
+//			if (Input.GetButtonDown ("Jump")) {
+//				ball.AddForce (Vector3.up * jumpForce, ForceMode.Impulse);
+//				onGround = false;
+//			}
+//		
+//		}
+//		float movemHor = Input.GetAxis ("Horizontal");
+//		float moveVer = Input.GetAxis ("Vertical");
+//		Vector3 movement = new Vector3 (movemHor, 0, moveVer);
+//		ball.AddForce (speed*movement);
+		if (ball.velocity.magnitude != 0f) {
+			forwardDir = Vector3.Normalize (ball.velocity);
+			leftDir = Vector3.Cross (forwardDir, Vector3.up);
+			rightDir = -1f * leftDir;
+			Debug.Log (ball.velocity.magnitude.ToString ());
 		}
-		float movemHor = Input.GetAxis ("Horizontal");
-		float moveVer = Input.GetAxis ("Vertical");
-		Vector3 movement = new Vector3 (movemHor, 0, moveVer);
-		ball.AddForce (speed*movement);
+			
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			ball.AddForce (force * (forwardDir));
+			Debug.Log ("up key is pressed");
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			ball.AddForce (-force * (forwardDir));
+			Debug.Log ("down key is pressed");
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			ball.AddForce (force * (rightDir));
+			Debug.Log ("right key is pressed");
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			ball.AddForce (force * (leftDir));
+			Debug.Log ("left key is pressed");
+			Debug.Log (forwardDir.ToString ());
+		}
+
 		if (Input.GetButtonDown ("Jump") && onGround) {
 			//ball.AddForce (Vector3.up*jumpForce, ForceMode.Impulse);
 			onGround = false;
@@ -57,6 +92,10 @@ public class Ball : MonoBehaviour {
 			Application.LoadLevel (scene.name);     // restart the game whene the player is falling
 			Debug.ClearDeveloperConsole ();
 		}
+
+		Debug.DrawRay (transform.position, forwardDir, Color.red);
+		Debug.DrawRay (transform.position, rightDir, Color.blue);
+		Debug.DrawRay (transform.position, leftDir, Color.yellow);
 	}
 
 
@@ -70,9 +109,13 @@ public class Ball : MonoBehaviour {
 
 	void OnCollisionEnter(Collision coll)
 	{
-		if (col.transform.tag == "cube" || col.gameObject.name == "Plane") {
+		if (coll.transform.tag == "cube" || coll.gameObject.name == "Plane") {
 			onGround = true;
 			//Debug.Log ("on ground");
 		}	
+	}
+
+	public Vector3 GetForwardDir(){
+		return forwardDir;
 	}
 }
